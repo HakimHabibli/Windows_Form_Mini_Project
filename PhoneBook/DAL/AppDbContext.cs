596bb3f3c52bd87782b5c0b59d299e5d16c3c861
HakimHabibli <HakimHabibli>
2024-08-05 20:08:32 +0400

    first commit

---
+++ PhoneBook/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Models;
+
+namespace PhoneBook.DAL;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=ASUS\SQLEXPRESS;database=PhoneBook;Trusted_Connection=True;TrustServerCertificate=True");	
+
+    }
+}
